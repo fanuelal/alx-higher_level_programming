4deda05241a4dfde24b056d5e6548f495c3552ce
fanuelal <fanuelalmawstop@gmial.com>
2021-09-07 18:12:57 +0000

    print comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 for i in range(0, 100):
-    if i < 99:
-        print("{:02d}".format(i),end = ", ")
-        continue
-    print(i,"\n")
+    if i == 99:
+        print(i)
+    else:
+        print("{:0>2d}".format(i), end=", ")
