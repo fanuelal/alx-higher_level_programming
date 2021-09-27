fa02c003b8f57e5bbd15f7a3b5eb2f77ebd85411
fanuelal <fanuelalmawstop.com>
2021-09-27 14:15:10 -0700

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    for j in range(0, x):
+    count = 0
+    for i in range(0, x):
         try:
-            print("{:d}".format(my_list[j]), end="")
-            i += 1
-        except:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except Exception:
             pass
     print()
-    return i
+    return count
