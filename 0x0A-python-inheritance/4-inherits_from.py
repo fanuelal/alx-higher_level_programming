37f04a1b8f17d420faa6641a283c9a3ba5d733e6
fanuelal <fanuelalmawstop.com>
2021-10-12 00:40:28 -0700

    inherit from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines the inherits_from function"""
 
+
 def inherits_from(obj, a_class):
     """returns True or False based on inheritance"""
     if isinstance(obj, a_class) is True and type(obj):
