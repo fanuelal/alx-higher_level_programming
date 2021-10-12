92e223e9279a00b56ac254ae7c55af03738743b8
fanuelal <fanuelalmawstop.com>
2021-10-12 07:24:00 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,10 @@
 """Load, add, save module"""
 
 
+from sys import argv
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
-from sys import argv
+
 
 filename = "add_item.json"
 
