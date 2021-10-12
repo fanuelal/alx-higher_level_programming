4869a9298f900345f9035b5466a0f452cc077f1c
fanuelal <fanuelalmawstop.com>
2021-10-12 00:42:37 -0700

    inherite from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """returns True or False based on inheritance"""
-    if isinstance(obj, a_class) is True and type(obj):
+    if isinstance(obj, a_class) and type(obj):
         return True
     else:
         return False
