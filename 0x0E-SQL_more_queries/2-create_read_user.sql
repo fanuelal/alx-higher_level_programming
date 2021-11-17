1b3f65f9f4d822c6d3f8d8d038d5e70167fde118
fanuelal <fanuelalmawstop.com>
2021-11-17 13:33:36 -0800

    create and read

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,7 @@
---script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creation of 0d_2  database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a user if not exists no error
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to a user
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
