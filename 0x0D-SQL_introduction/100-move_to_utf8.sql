f3182317e43d904e9b5a14230d9da792d1a363bc
fanuelal <fanuelalmawstop.com>
2021-11-17 11:52:45 -0800

    utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,4 +4,4 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- converts table to utf8
 ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- converts a field in a table to utf8
-ALTER TABLE hbtn_0c_0.first_table MODIFY `name` VARCHAR(256) DEFAULT '' NOT NULL;
+ALTER TABLE hbtn_0c_0.first_table MODIFY `name` VARCHAR(256) DEFAULT NULL;
