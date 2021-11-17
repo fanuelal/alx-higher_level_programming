8d8536800cdf414e33bea7d29f5884e86e1906c3
fanuelal <fanuelalmawstop.com>
2021-11-17 01:33:28 -0800

    list values

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
+SELECT * FROM first_table;
