151b213ad13b6f3b4d199ca8879e96fc549b9d7c
fanuelal <fanuelalmawstop.com>
2021-11-17 11:31:46 -0800

    number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,4 +2,4 @@
 -- the same score in the table second_table of the database
 -- hbtn_0c_0 in your MySQL server.
 
-SELECT score, COUNT(score)'number' FROM second_table GROUP BY score ORDER BY 'number' DESC;  
+SELECT score, COUNT(score)'number' FROM second_table GROUP BY score ORDER BY `number` DESC;  
