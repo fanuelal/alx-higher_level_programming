59ba71c75f127e23f52a00a45c73a303e455166b
fanuelal <fanuelalmawstop.com>
2021-11-18 05:12:19 -0800

    cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+--  script that creates the database hbtn_0d_usa and the table cities
+-- creating DB
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- using DB
+USE hbtn_0d_usa;
+-- creating table
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENCES states(id));
