8347c9bb18029b825f34a6a6a7736ba843e18317
fanuelal <fanuelalmawstop@gmail.com>
2021-12-13 16:05:40 +0000

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing size');
+}
 for (let i = 0; i < process.argv[2]; i++) {
   console.log('X'.repeat(process.argv[2]));
 }
