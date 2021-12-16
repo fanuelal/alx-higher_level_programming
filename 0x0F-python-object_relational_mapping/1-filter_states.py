a50efcc1c33a7c0bc3db23eeb2cb4487625121fa
fanuelal <fanuelalmawstop@gmial.com>
2021-12-16 19:37:06 +0000

    start with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host = 'localhost', port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    datas = cur.fetchall()
+    for data in datas:
+        print(data)
