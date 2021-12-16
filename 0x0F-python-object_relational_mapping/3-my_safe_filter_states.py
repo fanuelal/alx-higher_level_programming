f38ebfde5ea89f54e53825738d0e505b9adc847d
fanuelal <fanuelalmawstop@gmial.com>
2021-12-16 20:44:28 +0000

    safe filter states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a script that lists all states with a name starting with N (upper N) from th\
+\
+e database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host = 'localhost', port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id ASC", (sys.argv[4], ))
+    datas = cur.fetchall()
+    for data in datas:
+        print(data)
