5724fcb3970b1d57670054dad254a4d914364961
fanuelal <fanuelalmawstop@gmial.com>
2021-12-18 23:19:25 +0000

    search

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write a script that lists all State objects that contain the letter a from t\
+he database hbtn_0e_6_usa"""
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    searched =  session.query(State).filter(State.name == sys.argv[4]).first()
+    if searched:
+        print('{}'.format(searched.id))
+    else:
+        print('Not found')
+    session.close()
