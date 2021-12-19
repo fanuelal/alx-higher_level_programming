17f3e6b9210bd80f0cf409f2581d8eca7d40a7eb
fanuelal <fanuelalmawstop@gmial.com>
2021-12-19 00:13:24 +0000

    delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a script that changes the name of a State object from the database hbtn_0e_6\
+_usa t\
+\
+\
+he database hbtn_0e_6_usa"""
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).filter(State.name.like('%a%')):
+        session.delete(state)
+    session.commit()
+    session.close()
