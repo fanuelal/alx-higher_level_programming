05aabec83dbf543932961988c0154740aca152a3
fanuelal <fanuelalmawstop@gmial.com>
2021-12-19 00:06:55 +0000

    update

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
-
+    from sqlalchemy.schema import Table
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
     state = session.query(State).filter(State.id == 2).first()
-    State.name = 'New Mexico'
+    if state:
+        state.name = 'New Mexico'
     session.commit()
     session.close()
