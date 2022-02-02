cb5f5c395fbeb538f6c8710db207a003e0254286
fanuelal <fanuelalmawstop@gmail.com>
2022-02-02 15:07:17 -0800

    starwars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request({ url: url, json: true }, (err, res) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const films = res.body.results;
+    let count = 0;
+    for (let f = 0; f < films.length; f++) {
+      for (let c = 0; c < films[f].characters.length; c++) {
+        if (films[f].characters[c].includes('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
