9bf2b5ee579fd83b57d8236218d1415c2642e17b
fanuelal <fanuelalmawstop@gmail.com>
2022-02-02 14:30:47 -0800

    statuscode

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, response) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('code:', response.statusCode);
+});
