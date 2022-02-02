66e095aab047e9f645013ea9ee5ff262becedda2
fanuelal <fanuelalmawstop@gmail.com>
2022-02-02 13:38:15 -0800

    read me js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(process.argv[2],'utf8', function(err, data){
-    if (err){
-	return console.log(err);
-    }
-    console.log(data.toString());
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data.toString());
 });
