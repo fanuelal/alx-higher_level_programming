216eda45861165a1b2967f2fce29d033599235bc
fanuelal <fanuelalmawstop@gmail.com>
2022-02-02 15:04:43 -0800

    starwar id title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,10 +3,10 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
 
-request({url: url, json: true}, function (err, res) {
+request({ url: url, json: true }, function (err, res) {
   if (err) {
     console.log(err);
-  }else{
-      console.log(res.body.title);
+  } else {
+    console.log(res.body.title);
   }
 });
